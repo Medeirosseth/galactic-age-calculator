d601119a989039b80586605a68fc5e1e8d89a3db
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-16 14:43:42 -0700

    add refactored spaceAge constructor

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -2,24 +2,12 @@ import { SpaceAge } from "./../src/js/galactic-age";
 
 describe(`SpaceAge`, () => {
   it("creates a SpaceAge constructor object", () => {
-    const spaceAge = new SpaceAge("buzz", 30, 78, 15, 7, 5, 2, -10);
+    const spaceAge = new SpaceAge("buzz", 30, 78);
     const expected = {
       name: `buzz`,
-      earthAge: 1,
-      mercury: 15,
-      venus: 7,
-      mars: 5,
-      jupiter: 2,
-      smoker: -10,
+      earth: 30,
+      aveLifeExpectancy: 78,
     };
     expect(spaceAge).toEqual(expected);
-  });
-
-  it("should convert earthAge into earthDays ", () => {
-    earthDays();
-    {
-      const earthDays = this.earthAge * 365;
-      expect(earthDays(1)).toEqual(365);
-    }
   });
 });
