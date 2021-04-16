0cf18ee406e19bd2c1c03e01149d1b77c29e8295
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-16 10:09:33 -0700

    add good fail

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -1,5 +1,15 @@
 import { SpaceAge } from "./../src/js/galactic-age";
 
-describe("SpaceAge()", () => {
-  let;
+describe(`SpaceAge()`, () => {
+  it(`creates a space age converter converter`, () => {
+    const expected = {
+      earth: 1,
+      mercury: "mercury years",
+      venus: "Venus Years",
+      mars: "Mars Years",
+      jupiter: "Jupiter Years",
+      smoker: "Smoker years",
+    };
+    expect(new SpaceAge()).toEqual(expected);
+  });
 });
--- src/js/galactic-age.js
+++ src/js/galactic-age.js
@@ -1,3 +1,10 @@
 export class SpaceAge {
   constructor() {}
 }
+/// A class that takes a persons age in human years and calculates their
+/// age depending on a corrosponding planet
+/// mercury is .24 Earth years
+/// venus is .62 Earth years
+/// mars is .1.88 Earth years
+/// jupiter is 11.86 Earth years
+/// user inputs whether or not they are a smoker, smokers life expectancy is 10 years short than a non smoker
