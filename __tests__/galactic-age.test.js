aea476dbf2b0791ac4ba6de1880976b9cbc5d25a
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-16 09:49:13 -0700

    add import and export

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -1,13 +1,5 @@
-import SpaceAge from "../src/galactic-age";
+import { SpaceAge } from "./../src/js/galactic-age";
 
-/// A class that takes a persons age in human years and calculates their
-/// age depending on a corrosponding planet
-/// mercury is .24 Earth years
-/// venus is .62 Earth years
-/// mars is .1.88 Earth years
-/// jupiter is 11.86 Earth years
-/// user inputs whether or not they are a smoker, smokers life expectancy is 10 years short than a non smoker
-
-describe("spaceage()", () => {
+describe("SpaceAge()", () => {
   let;
 });
--- src/galactic-age.js
+++
@@ -1,3 +0,0 @@
-export default class SpaceAge {
-  constructor() {}
-}
