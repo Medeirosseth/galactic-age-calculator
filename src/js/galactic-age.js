052bf0da64139f3ed3c88e0f9f30e1e432f78ba6
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-16 11:45:17 -0700

    add plain english  description of tacking project

--- src/js/galactic-age.js
+++ src/js/galactic-age.js
@@ -1,14 +1,34 @@
 export class SpaceAge {
-  constructor(name, earth, mercury, venus, mars, jupiter, smoker) {
+  constructor(
+    name,
+    earth,
+    mercury,
+    venus,
+    mars,
+    jupiter,
+    aveLifeExpectancy,
+    smoker
+  ) {
     this.name = name;
     this.earth = earth;
     this.mercury = mercury;
     this.venus = venus;
     this.mars = mars;
     this.jupiter = jupiter;
+    this.aveLifeExpectancy = aveLifeExpectancy;
     this.smoker = smoker;
   }
 }
+/// user opens website
+/// inputs name and earth age
+/// returns name and earth age along with converted planet years
+/// template literals would be a good way about returning each year
+/// create an age input that breaks down age to days * 365(in a year)
+/// a mercury year is 88 earth days
+/// a venus year is 224.7 earth days
+/// a mars year is is 687 earth days
+/// a jupiter year is 4,332.59 earth days
+/// a smoker on average lives 10 fewer years
 /// A class that takes a persons age in human years and calculates their
 /// age depending on a corrosponding planet
 /// mercury is .24 Earth years
